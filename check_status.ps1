b77b34386625320fd84170fcac3548e901c83828
Jimmbo254 <jaimeescalante254@gmail.com>
2026-02-08 02:15:21 -0700

    Script PS check_status

--- check_status.ps1
+++ check_status.ps1
@@ -4,4 +4,4 @@ Write-Host "Equipo: $env:COMPUTERNAME"
 Write-Host "IPs:"
 Get-NetIPAddress -AddressFamily IPv4 | Select-Object IPAddress
 $disco = Get-PSDrive C
-Write-Host "Disco C: Libre $([Math]::Round($disco.Free/1GB,2)) GB"
+Write-Host "Disco C: Libre $([Math]::Round($disco.Free/1GB,2)) GB."
