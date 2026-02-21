c929c0f4268ad223d025e21cbb4d472fa5bdf8c5
Jimmbo254 <jaimeescalante254@gmail.com>
2026-02-21 03:17:07 -0700

    Movimiento check_status.ps1 y borrar duplicado

--- scripts/check_status.ps1
+++ scripts/check_status.ps1
@@ -4,5 +4,4 @@ Write-Host "Equipo: $env:COMPUTERNAME"
 Write-Host "IPs:"
 Get-NetIPAddress -AddressFamily IPv4 | Select-Object IPAddress
 $disco = Get-PSDrive C
-Write-Host "Disco C: Libre $([math]::Round($disco.Free/1GB,2)) GB"
-Pause
+Write-Host "Disco C: Libre $([Math]::Round($disco.Free/1GB,2)) GB."
