7dab1a2d9213407169de2068e17c1f9525cc2d76
Jimmbo254 <jaimeescalante254@gmail.com>
2026-02-23 21:49:57 -0700

    menu sin configuracion dhcp activa

--- Windows/dhcp_windows.ps1
+++ Windows/dhcp_windows.ps1
@@ -18,3 +18,33 @@ function Menu-DHCP {
     Write-Host "5) Salir"
     return Read-Host "`nSeleccione una opcion"
 }
+
+do {
+    $opcion = Menu-DHCP
+    switch ($opcion) {
+        "1" {
+            $status = Get-WindowsFeature DHCP
+            Write-Host "`nEstado Instalacion: $($status.InstallState)" -ForegroundColor Yellow
+            Pause
+        }
+        "2" {
+            $status = Get-WindowsFeature DHCP
+            $accion = Read-Host "'I' para Instalar o 'D' para Desinstalar"
+            if ($accion -eq 'I') {
+                if ($status.InstallState -eq "Installed") { Write-Host "Ya instalado." -ForegroundColor Yellow }
+                else { Install-WindowsFeature DHCP -IncludeManagementTools }
+            }
+            elseif ($accion -eq 'D') { Uninstall-WindowsFeature DHCP -IncludeManagementTools }
+            Pause
+        }
+        "3" {
+        }
+        "4" {
+            Get-DhcpServerv4Scope | ForEach-Object {
+                Write-Host "Red: $($_.ScopeId)" -ForegroundColor Yellow
+                Get-DhcpServerv4Lease -ScopeId $_.ScopeId | Select-Object IPAddress, HostName, LeaseExpiryTime | Format-Table -AutoSize
+            }
+            Pause
+        }
+    }
+} while ($opcion -ne "5")
