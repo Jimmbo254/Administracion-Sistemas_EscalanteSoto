3227c74018bd4f08f4bed8c22ab0593d6a2333f6
Jimmbo254 <jaimeescalante254@gmail.com>
2026-02-23 21:45:45 -0700

    Creacion Menu Windows

--- Windows/dhcp_windows.ps1
+++ Windows/dhcp_windows.ps1
@@ -2,3 +2,19 @@ function Validar-IP ($ip) {
 	if ($ip -eq "0.0.0.0" -or $ip -eq "255.255.255.255" -or $ip -eq "127.0.0.1") { return $false }
 	return $ip -match "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
 }
+
+function IP-A-Numero ($ip) {
+    $octetos = $ip.Split('.')
+    return [double]($octetos[0]) * [math]::Pow(256, 3) + [double]($octetos[1]) * [math]::Pow(256, 2) + [double]($octetos[2]) * 256 + [double]($octetos[3])
+}
+
+function Menu-DHCP {
+    Clear-Host
+    Write-Host " ===  Servicio DHCP Windows ===" -ForegroundColor Cyan
+    Write-Host "1) Verificar status DHCP"
+    Write-Host "2) Instalar/Desinstalar"
+    Write-Host "3) Configurar Servidor"
+    Write-Host "4) Monitorear "
+    Write-Host "5) Salir"
+    return Read-Host "`nSeleccione una opcion"
+}
