4770616d7819830c59653e215525e2df19262e3a
Jimmbo254 <jaimeescalante254@gmail.com>
2026-02-26 05:32:48 -0700

    estructura Menu y Funciones dns_windows

--- Windows/dns_windows.ps1
+++ Windows/dns_windows.ps1
@@ -1,3 +1,58 @@
 # dns_windows.ps1 - Servicio DNS
 
-#INICIALIZACION Y CREACION DE ARCHIVO
+. ".\funciones_windows.ps1"
+
+# FUNCIONES DNS
+
+function Verificar-DNS {
+    Write-Host ""
+    Write-Host "=== Verificar instalacion DNS ==="
+}
+
+function Instalar-DNS {
+    Write-Host ""
+    Write-Host "==== Instalando dependencias DNS ===="
+}
+
+function Listar-Dominios {
+    Write-Host ""
+    Write-Host "=== Dominios configurados ==="
+}
+
+function Agregar-Dominio {
+    Write-Host ""
+    Write-Host "=== Agregar nuevo dominio ==="
+}
+
+function Eliminar-Dominio {
+    Write-Host ""
+    Write-Host "=== Eliminar dominio ==="
+}
+
+# MENU MAIN
+
+Verificar-Administrador
+
+function Menu-DNS {
+    Clear-Host
+    Write-Host "========= MENÚ DNS =========" -ForegroundColor Cyan
+    Write-Host "1) Verificar instalacion"
+    Write-Host "2) Instalar dependencias"
+    Write-Host "3) Listar Dominios configurados"
+    Write-Host "4) Agregar nuevo dominio"
+    Write-Host "5) Eliminar un dominio"
+    Write-Host "6) Salir"
+    Write-Host "============================="
+    return Read-Host "`nSelecciona una opcion"
+}
+
+do {
+    $opcion = Menu-DNS
+    switch ($opcion) {
+        "1" { Verificar-DNS    }
+        "2" { Instalar-DNS     }
+        "3" { Listar-Dominios  }
+        "4" { Agregar-Dominio  }
+        "5" { Eliminar-Dominio }
+    }
+} while ($opcion -ne "6")
