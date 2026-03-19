07c46a6c94c8e66364739b4530605d8d7b8fe2bf
Jimmbo254 <jaimeescalante254@gmail.com>
2026-03-19 00:58:35 -0700

    Update http_windows.ps1

--- Windows/http_windows.ps1
+++ Windows/http_windows.ps1
@@ -9,7 +9,8 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
 }
 
 do {
-    Write-Host "`n======= MENU WINDOWS =======" -ForegroundColor Green
+    Write-Host ""
+    Write-Host "========= MENU HTTP =========" -ForegroundColor Blue
     Write-Host "1) Instalar IIS"
     Write-Host "2) Instalar Apache"
     Write-Host "3) Instalar Nginx"
@@ -17,17 +18,19 @@ do {
     Write-Host "5) Desinstalar Apache"
     Write-Host "6) Desinstalar Nginx"
     Write-Host "0) Salir"
-    
-    $opcion = Read-Host "Elige una opcion"
+    Write-Host "============================="
+    Write-Host ""
+
+    $opcion = Read-Host "Selecciona una opcion"
 
     switch ($opcion) {
-        "1" { Instalar-IIS }
-        "2" { Instalar-Opcional -Servicio "apache" }
-        "3" { Instalar-Opcional -Servicio "nginx" }
-        "4" { Desinstalar-IIS }
-        "5" { Desinstalar-Opcional -Servicio "apache" }
-        "6" { Desinstalar-Opcional -Servicio "nginx" }
-        "0" { Write-Host "Saliendo del script..."; break }
-        default { Write-Host "[X] Opcion no valida." -ForegroundColor Red }
+        "1" { Instalar-IIS     }
+        "2" { Instalar-Apache  }
+        "3" { Instalar-Nginx   }
+        "4" { Desinstalar-IIS  }
+        "5" { Desinstalar-Apache }
+        "6" { Desinstalar-Nginx  }
+        "0" { Write-Host "Cerrando..."; break }
+        default { Write-Host "  [Error] Opcion no valida." -ForegroundColor Red }
     }
 } while ($opcion -ne "0")
